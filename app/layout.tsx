117561031f3b407931bfdca7fcd409fb4b1b60ee
Sanjog Limbu <terrencetao0@gmail.com>
2026-05-24 00:55:19 +0100

    Added Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import "./globals.css";
-import TopNav from "./components/TopNav"
-import Providers from "./components/Providers"
 import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/next"
+import Providers from "./components/Providers"
+import TopNav from "./components/TopNav"
+
+//
 
 const inter = Inter({
     subsets: ['latin'],
@@ -10,22 +13,18 @@ const inter = Inter({
 
 //
 
-export default function RootLayout({
-children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" className={`${inter.variable} font-sans`}>
-      <title>SkinSlinger</title>
-
-      <body className="h-screen w-[95%] mx-auto flex flex-col overflow-hidden">
-            <Providers>
-                <TopNav />
-                {children}
-            </Providers>
-        </body>
-    </html>
-  );
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+    return (
+        <html lang="en" className={`${inter.variable} font-sans`}>
+            <title>SkinSlinger</title>
+            <body className="h-screen w-[95%] mx-auto flex flex-col overflow-hidden">
+                <Providers>
+                    <TopNav />
+                    {children}
+                </Providers>
+                
+                <Analytics />
+            </body>
+        </html>
+    );
 }
-
