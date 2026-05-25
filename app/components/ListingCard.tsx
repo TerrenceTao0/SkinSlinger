24a48f56ea8e163ddeaf3b2652e2cc5cf1e168e1
Sanjog Limbu <terrencetao0@gmail.com>
2026-05-25 15:00:08 +0100

    Display fixes

--- app/components/ListingCard.tsx
+++ app/components/ListingCard.tsx
@@ -23,7 +23,7 @@ export default function ListingCard(
 
     return (
         <div
-            className="bg-accent h-50 w-full rounded-sm relative transition-all duration-200 hover:scale-[1.04] hover:-translate-y-1 hover:z-10 hover:[box-shadow:0_8px_20px_var(--glow),0_4px_10px_rgba(0,0,0,0.5)]"
+            className="bg-accent h-50 w-full md:w-49 rounded-sm relative transition-all duration-200 hover:scale-[1.04] hover:-translate-y-1 hover:z-10 hover:[box-shadow:0_8px_20px_var(--glow),0_4px_10px_rgba(0,0,0,0.5)]"
             style={{ '--glow': `#${hexColor}44` } as React.CSSProperties}
         >
             <div className="flex justify-between mt-2 pl-2 pr-2 w-full absolute z-2">
